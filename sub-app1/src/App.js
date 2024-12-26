16de4f2f5de88b5b914aea236800f13332ada372
rashedabir <rashedabir.cse@gmail.com>
2024-12-26 17:16:00 +0600

    first commit

---
+++ sub-app1/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={`${process.env.REACT_APP_CONTENT_HOST}${logo}`}
+          className="App-logo"
+          alt="logo"
+        />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Frontend 1
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
